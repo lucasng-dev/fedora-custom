bac6615e17a400f66bf7af041f624618148700ec
Lucas NG <lucasng.dev@gmail.com>
2025-12-02 23:42:28 -0300

    feat: update scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -120,7 +120,7 @@ mkdir canon && bsdtar -xof canon.tar.gz -C canon --strip-components=1
 dnf install -y canon/packages/cnijfilter2-*.x86_64.rpm
 
 # disable 3rd party repos
-sed -Ei '/^enabled=/c\enabled=0' /etc/yum.repos.d/{google-chrome,brave-browser,tailscale,cloudflared,1password}.repo
+sed -Ei '/^enabled=/c\enabled=0' /etc/yum.repos.d/{google-chrome,brave-browser,tailscale,cloudflared,1password,vscode}.repo
 
 # post-install (1password)
 rm -f /usr/lib/sysusers.d/*onepassword*.conf &>/dev/null || true
