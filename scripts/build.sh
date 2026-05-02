ba30da812d7574f8eda89c8063fd2c384d864217
Lucas NG <lucasng.dev@gmail.com>
2026-05-02 10:00:13 -0300

    feat: update scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,8 +11,8 @@ rm -vf /etc/yum.repos.d/{rpmfusion-*,_copr:*}.repo
 
 # enable rpm fusion repos: https://rpmfusion.org/Configuration
 dnf install -y \
-	"https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
-	"https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+	"https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+	"https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 dnf config-manager setopt fedora-cisco-openh264.enabled=1
 dnf install -y 'rpmfusion-*-appstream-data'
 
